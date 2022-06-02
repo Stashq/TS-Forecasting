1478949fcf96dfc2ee489fb1763170ee9c09d0d5
Stanisław Straburzyński <sstraburzynski@gmail.com>
2022-06-02 18:54:22 +0200

    Adding ConvAEWrapper and MultipleConvAE

--- models/__init__.py
+++ models/__init__.py
@@ -2,4 +2,4 @@
 from .lstm_ae import (
     LSTMEncoder, LSTMDecoder, LSTMAE, LSTMVAE, LSTMPAE, LSTMPVAE)
 from .conv_ae import (
-    ConvEncoder, ConvDecoder, ConvAE, ConvVAE)
+    ConvEncoder, ConvDecoder, ConvAE, ConvVAE, MultipleConvAE, ConvAEWrapper)
